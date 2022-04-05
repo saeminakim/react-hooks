3b35cd9c1459e8566baffcfb353f57bb174d0e8a
Lily <helloimlily@gmail.com>
2022-04-05 15:16:11 +0900

    #1.1 useInput

--- useInput/useInput.js
+++ useInput/useInput.js
@@ -1,7 +1,4 @@
-import React, { useState } from "react";
-import "./styles.css";
-
-const useInput = (initialValue, validator) => {
+export const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
   const onChange = (event) => {
     const {
@@ -17,14 +14,3 @@ const useInput = (initialValue, validator) => {
   };
   return { value, onChange };
 };
-
-export default function App() {
-  const maxLen = (value) => value.length <= 10;
-  const name = useInput("Mr.", maxLen);
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      <input placeholder="Name" {...name} />
-    </div>
-  );
-}
