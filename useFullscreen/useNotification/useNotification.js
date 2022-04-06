d7d97bec7a6b6c90b6c2fa5b042d662461d7db0a
Lily <helloimlily@gmail.com>
2022-04-06 14:35:03 +0900

    #2.7 useNotification

---
+++ useFullscreen/useNotification/useNotification.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
+  }
+  const fireNotif = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
+    }
+  };
+  return fireNotif;
+};
+
+export default function App() {
+  const triggerNotif = useNotification("Let me get out of here", {
+    body: "lalalala",
+  });
+  return (
+    <div className="App">
+      <button onClick={triggerNotif}>hello</button>
+    </div>
+  );
+}
