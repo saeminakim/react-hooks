6ebdb6c7f19732d549b469713b49f97b13790fcb
Lily <helloimlily@gmail.com>
2022-04-06 10:07:24 +0900

    #2.3 useConfirm

---
+++ useConfirm/useConfirm.js
@@ -0,0 +1,24 @@
+const useConfirm = (message = "", callback, rejection) => {
+  if (typeof callback !== "function") {
+    return;
+  }
+  const confirmAction = () => {
+    if (confirm(message)) {
+      callback();
+    } else {
+      rejection();
+    }
+  };
+  return confirmAction;
+};
+
+export default function App() {
+  const deleteWorld = () => console.log("Deleted");
+  const abort = () => console.log("Aborted");
+  const confirmDelete = useConfirm("Are you sure?", deleteWorld, abort);
+  return (
+    <div className="App">
+      <button onClick={confirmDelete}>Delete</button>
+    </div>
+  );
+}
