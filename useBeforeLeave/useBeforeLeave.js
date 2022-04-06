90e4ee0a5cd8bc024c4e3ed7322fce8e5fbfd52f
Lily <helloimlily@gmail.com>
2022-04-06 10:56:37 +0900

    #2.4 useBeforeLeave

---
+++ useBeforeLeave/useBeforeLeave.js
@@ -0,0 +1,25 @@
+export const useBeforeLeave = (onBefore) => {
+  if (typeof onBefore !== "function") {
+    return;
+  }
+  const handle = (event) => {
+    const { clientY } = event;
+    if (clientY <= 0) {
+      onBefore();
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("mouseleave", handle);
+    return () => document.removeEventListener("mouseleave", handle);
+  }, []);
+};
+
+export default function App() {
+  const begForLife = () => console.log("Don't leave");
+  useBeforeLeave(begForLife);
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
--- useClick/useClick.js
+++ useClick/useClick.js
@@ -15,6 +15,7 @@ export const useClick = (onClick) => {
       }
     };
   }, []);
+
   return element;
 };
 
