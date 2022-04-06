ad014904444afaa3b2b8ecd36d72fedcf2457fbf
Lily <helloimlily@gmail.com>
2022-04-06 09:57:13 +0900

    #2.2 useClick

---
+++ useClick/useClick.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from "react";
+
+const useClick = (onClick) => {
+  if (typeof onClick !== "function") {
+    return;
+  }
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return element;
+};
+
+export default function App() {
+  const sayHello = () => console.log("helloooo");
+  const title = useClick(sayHello);
+  return (
+    <div className="App">
+      <h1 ref={title}>Hi</h1>
+    </div>
+  );
+}
